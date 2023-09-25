edbefc8d3d2c11ed5ef7d06d31e4bbdd6a5478a6
Anthony <arcferrer5@gmail.com>
2023-09-25 10:55:44 +0800

    Add google login auth

--- nutrition-backend/config/routes.rb
+++ nutrition-backend/config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post 'user/google' => 'users#google' # This maps the /user/google route to the UsersController's google action
 end
