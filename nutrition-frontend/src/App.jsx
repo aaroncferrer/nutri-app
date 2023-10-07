310c19c9c7a384d883643e01705226eceb53aa9b
Anthony <arcferrer5@gmail.com>
2023-10-07 14:51:11 +0800

    Add services comp in landing

--- nutrition-frontend/src/App.jsx
+++ nutrition-frontend/src/App.jsx
@@ -10,7 +10,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'aos/dist/aos.css';
 import AOS from 'aos';
 import Auth from "./pages/Auth";
-// import axios from 'axios';
 
 function App() {
 	const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem('currentUser')) || null)
@@ -22,34 +21,9 @@ function App() {
 	useEffect(() => {
 		AOS.init({duration: 1000, anchorPlacement: 'center-bottom'})
 	}, [])
-	// const [eventTypes, setEventTypes] = useState([]);
 
-	// useEffect(() => {
-	// 	const fetchEventTypes = async () => {
-	// 	try {
-	// 		const response = await axios.get('http://localhost:3000/get_user_event_types');
-	// 		setEventTypes(response.data.collection);
-	// 		console.log(response.data.collection)
-	// 	} catch (error) {
-	// 		console.error('Error fetching event types:', error);
-	// 	}
-	// 	};
-	// 	fetchEventTypes();
-	// }, []); 
 
 	return (
-		// <>
-		// 	<p>Select a scheduling link:</p>
-		// 	<ul>
-		// 		{eventTypes.map((eventType) => (
-		// 		<li key={eventType.uri}>
-		// 			<a href={eventType.scheduling_url} target="_blank" rel="noopener noreferrer">
-		// 			{eventType.name}
-		// 			</a>
-		// 		</li>
-		// 		))}
-		// 	</ul>
-		// </>
 		<Router>	
 			<NavHeader />
 			<Routes>
--- nutrition-frontend/src/pages/Landing/index.jsx
+++ nutrition-frontend/src/pages/Landing/index.jsx
@@ -1,11 +1,13 @@
 import LandingHeader from "../../components/01_LandingHeader";
 import LandingAbout from "../../components/02_LandingAbout";
+import LandingServices from "../../components/03_LandingServices";
 
 function Landing() {
     return(
         <main className="landing">
             <LandingHeader />
             <LandingAbout />
+            <LandingServices />
         </main>
     )
 }
