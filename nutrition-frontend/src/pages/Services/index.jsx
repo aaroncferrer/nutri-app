2d4bf65d4331c545d4ab8645c59ff736a83778e5
Anthony <arcferrer5@gmail.com>
2023-10-10 11:55:52 +0800

    Add services page

---
+++ nutrition-frontend/src/pages/Services/index.jsx
@@ -0,0 +1,51 @@
+import { useEffect } from 'react';
+import './services.css';
+import servicesData from './services.json';
+
+function Services({currentUser}) {
+    useEffect(() => {
+        window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+    }, []);
+
+    const services = servicesData.services;
+    console.log(services)
+
+    return(
+        <>
+            <main className='services'>
+                <section className="services_header">
+                    <h1>services</h1>
+                </section>
+
+                <section className="services_card_container">
+                    {services.map((service => (
+                        <article className="service_card" key={service.id}>
+                            <div className="service_img_container">
+                                <img src={service.image} className='service_img'></img>
+                            </div>
+                            <div className="service_text">
+                                <h4>{service.name}</h4>
+                                <h6>{service.duration} MINUTES</h6>
+                                <span>{service.note ? `*${service.note}` : null}</span>
+                                <div className="service_line"></div>
+                                <p>{service.description_01}</p>
+                                <p>{service.description_02}</p>
+                                <p>{service.description_03}</p>
+                                <div className="service_line"></div>
+                                <a
+                                    className={`${currentUser === null ? 'disabled' : 'custom_btn'} service_btn  `}
+                                    href={service.scheduling_url}
+                                    disabled={currentUser === null}
+                                >
+                                    {currentUser === null ? "LOGIN TO SCHEDULE" : "SCHEDULE NOW"}
+                                </a>
+                            </div>
+                        </article>
+                    )))}
+                </section>
+            </main>
+        </>
+    )
+}
+
+export default Services;
